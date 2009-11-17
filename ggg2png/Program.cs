5428f74a8a9ef31c693065c33f542c59577bbd88
Mikhail Kozhevnikov <mikhailkozhevnikov@yandex.ru>
2009-11-17 15:32:46 +0300

    ggg2npg

---
+++ ggg2png/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+namespace ggg2png
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string[] files = Directory.GetFiles(args[0]);
+         foreach (var file in files)
+         {
+            var path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "..\\pngs"));
+            var dstname = Path.Combine(path, Path.GetFileNameWithoutExtension(file) + ".png");
+            //var dstname = file.Replace(".raw", ".png");
+            FileStream fs = new FileStream(file, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            int width = br.ReadInt32();
+            int height = br.ReadInt32();
+            int xdpi = br.ReadInt32();
+            int ydpi = br.ReadInt32();
+            Bitmap bmp = new Bitmap(width, height);
+            for (int y = 0; y < height; ++y)
+               for (int x = 0; x < width; ++x)
+               {
+                  int b = (int)br.ReadByte();
+                  bmp.SetPixel(x, y, Color.FromArgb(b, b, b));
+               }
+            br.Close();
+            bmp.Save(dstname, System.Drawing.Imaging.ImageFormat.Png);
+         }
+      }
+   }
+}
---
+++ ggg2png/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ggg2png")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("itcwin")]
+[assembly: AssemblyProduct("ggg2png")]
+[assembly: AssemblyCopyright("Copyright © itcwin 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c952ac2-983c-415c-8577-dc1bbd6be2c1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
