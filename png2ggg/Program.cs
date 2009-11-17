d2e038c87a02ae493244898958789fead447d268
Mikhail Kozhevnikov <mikhailkozhevnikov@yandex.ru>
2009-11-17 15:33:27 +0300

    png2ggg

---
+++ png2ggg/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace png2ggg
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Bitmap img = Image.FromFile(args[0]) as Bitmap;
+         FileStream fs = new FileStream(args[1], FileMode.Create);
+         BinaryWriter bw = new BinaryWriter(fs);
+         bw.Write(img.Width);
+         bw.Write(img.Height);
+         bw.Write((int)img.HorizontalResolution);
+         bw.Write((int)img.VerticalResolution);
+
+         for (int y = 0; y < img.Height; ++y)
+            for (int x = 0; x < img.Width; ++x)
+               bw.Write((Byte)Math.Min(Math.Max(img.GetPixel(x, y).GetBrightness()*255,0),255));
+         bw.Close();
+      }
+   }
+}
---
+++ png2ggg/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("png2ggg")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("itcwin")]
+[assembly: AssemblyProduct("png2ggg")]
+[assembly: AssemblyCopyright("Copyright © itcwin 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d419c883-d7c0-4495-9aaa-a5214a7a792d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
